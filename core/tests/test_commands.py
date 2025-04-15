5f5a2c4ac2e2e056eaeca0a1293e35b829d7b04a
ZACHARIE  Abba Moussa <zachzic.zam@gmail.com>
2025-04-15 13:32:39 +0000

    flake8

--- core/tests/test_commands.py
+++ core/tests/test_commands.py
@@ -1,9 +1,7 @@
-""" Test custom Django management commands """
+"""Test custom Django management commands."""
 
 from unittest.mock import patch
-
 from psycopg2 import OperationalError as Psycopg2Error
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
@@ -18,8 +16,7 @@ class CommandTests(SimpleTestCase):
         patched_check.return_value = True
 
         call_command("wait_for_db")
-        
-        patched_check.assert_called_once_with(databases = ['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -30,4 +27,4 @@ class CommandTests(SimpleTestCase):
         call_command("wait_for_db")
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(databases = ['default'])
+        patched_check.assert_called_with(databases=['default'])
